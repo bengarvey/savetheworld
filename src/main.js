c83b5eca35b620724f26649b1dada745bb8e517b
Brian Sloane <bsloane@rjmetrics.com>
2014-10-22 21:48:54 -0400

    added some enemies and song

--- src/main.js
+++ src/main.js
@@ -4,26 +4,43 @@ function preload() {
 
     game.load.image('background','assets/tests/real-city.png');
     game.load.image('player','assets/sprites/phaser-dude.png');
-
+    game.load.audio('amia_dope_song', ['assets/amia_dope_song.m4a']);
 }
 
 var player;
 var cursors;
 var jumpTimer = 0;
+var worldsize = 5000;
 
 function create() {
-    game.add.tileSprite(0, 0, 672, 320, 'background');
+    game.add.tileSprite(0, 0, worldsize, 320, 'background');
 
-    game.world.setBounds(0, 0, 672, 320);
+    music = game.add.audio('amia_dope_song');
+    music.loop = true;
+    music.play();
+
+    game.world.setBounds(0, 0, worldsize, 320);
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.gravity.y = 1500;
 
-    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    player = game.add.sprite(0, game.world.centerY, 'player');
+    joker = game.add.sprite(5000, game.world.centerY, 'player');
+    catwoman = game.add.sprite(4000, game.world.centerY, 'player');
+    darthvader = game.add.sprite(3000, game.world.centerY, 'player');
+    alien = game.add.sprite(2000, game.world.centerY, 'player');
 
     game.physics.enable(player);
+    game.physics.enable(joker);
+    game.physics.enable(catwoman);
+    game.physics.enable(darthvader);
+    game.physics.enable(alien);
 
     player.body.collideWorldBounds = true;
+    joker.body.collideWorldBounds = true;
+    catwoman.body.collideWorldBounds = true;
+    darthvader.body.collideWorldBounds = true;
+    alien.body.collideWorldBounds = true;
     cursors = game.input.keyboard.createCursorKeys();
 
     game.camera.follow(player);
@@ -31,20 +48,15 @@ function create() {
 
 function update() {
 
-    player.body.velocity.x = 0;
-
-    if (cursors.up.isDown)
+    player.body.velocity.x = 250;
+    joker.body.velocity.x = -245;
+    catwoman.body.velocity.x = -245;
+    darthvader.body.velocity.x = -245;
+    alien.body.velocity.x = -245;
+    
+    if (cursors.up.isDown && player.body.onFloor())
     {
         player.body.velocity.y = -500;
-    }
-
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -300;
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 300;
     }
 
 }
